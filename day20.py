c519d5ec4f43b652e71bb1b4b89693286e1f6c1a
animeshjain7 <108515181+animeshjain7@users.noreply.github.com>
2023-06-09 10:01:19 +0530

    cleaned

--- day20.py
+++ day20.py
@@ -1,8 +1,18 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def some_funcion():
-    return "some message from flask"
+    return '''
+    <html>
+    <head>
+    <title>home</title></head>
+    <body>
+    <h1>hello !!!!</h1>
+    </body>
+    </html>
+    '''
+
 
 app.run(debug=True)
