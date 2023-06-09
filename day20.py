ec568e4fd547c852ab653124ab708718813260ac
animeshjain7 <108515181+animeshjain7@users.noreply.github.com>
2023-06-09 19:42:11 +0530

    commit

--- day20.py
+++ day20.py
@@ -7,4 +7,11 @@ def some_funcion():
     return render_template('home.html')
 
 
+@app.route('/home/<name>')
+def some_funcion2(name):
+    print(name)
+    return render_template(
+        'home.html',name_value=name
+        )
+
 app.run(debug=True)
