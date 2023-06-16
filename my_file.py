c67e409d2572edbfacc1d55dff70a2270039202f
animeshjain7 <108515181+animeshjain7@users.noreply.github.com>
2023-06-16 10:39:56 +0530

    cleaned

--- my_file.py
+++
@@ -1,8 +0,0 @@
-#  dunder like ->   __anythng__ 
-if __name__ == '__main__':
-    print('from main of my_file.py file')
-
-# if we only run this file without importing then we can run line 3 only
-
-def abc():
-    print("im from abc() function of my_file.py")
--- newPack/first.py
+++
@@ -1,4 +0,0 @@
-import my_file
-import newPack.new1 as new1
-my_file.abc()
-new1.xyz()
